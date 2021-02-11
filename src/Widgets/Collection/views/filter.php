f572a4646b8d0f5db0c29e54038a09d5129400b7
Vitaliy <theyamshikov@yandex.ru>
2021-02-11 16:37:58 +0600

    upd

--- src/Widgets/Collection/views/filter.php
+++ src/Widgets/Collection/views/filter.php
@@ -17,19 +17,30 @@ use ZnLib\Web\Widgets\Format\Formatters\ActionFormatter;
 
 $propertyAccessor = PropertyAccess::createPropertyAccessor();
 
+$isExists = false;
+foreach ($attributes as $attributeEntity) {
+    $attributeName = $attributeEntity->getAttributeName();
+    $attributeNameCamelCase = Inflector::variablize($attributeName);
+    if (property_exists($filterModel, $attributeNameCamelCase)) {
+        $isExists = true;
+    }
+}
+
 ?>
 
-<form id="collection-filter-form" action="" method="get">
-    <tr>
-        <?php foreach ($attributes as $attributeEntity): ?>
-            <th>
-                <?php
-                $attributeName = $attributeEntity->getAttributeName();
-                $attributeNameCamelCase = Inflector::variablize($attributeName);
-                if ($attributeEntity->getFormatter()) {
-                    $formatter = ClassHelper::createObject($attributeEntity->getFormatter());
-                    if ($formatter instanceof ActionFormatter) {
-                        echo '
+<?php if($isExists): ?>
+
+    <form id="collection-filter-form" action="" method="get">
+        <tr>
+            <?php foreach ($attributes as $attributeEntity): ?>
+                <th>
+                    <?php
+                    $attributeName = $attributeEntity->getAttributeName();
+                    $attributeNameCamelCase = Inflector::variablize($attributeName);
+                    if ($attributeEntity->getFormatter()) {
+                        $formatter = ClassHelper::createObject($attributeEntity->getFormatter());
+                        if ($formatter instanceof ActionFormatter) {
+                            echo '
                         <div class="text-right">
                             <a class="text-decoration-none text-primary" href="#" onclick="filterForm.submit(); return false;" title="Send filter parameters">
                                 <i class="fas fa-filter"></i>
@@ -39,43 +50,45 @@ $propertyAccessor = PropertyAccess::createPropertyAccessor();
                                 <i class="fas fa-times"></i>
                             </a>
                         </div>';
-                    }
-                }
-                if (property_exists($filterModel, $attributeNameCamelCase)) {
-                    $value = $propertyAccessor->getValue($filterModel, $attributeNameCamelCase);
-                    $filterDefinition = $attributeEntity->getFilter();
-                    $isDefinition = is_array($filterDefinition) && isset($filterDefinition['class']) || is_string($filterDefinition);
-                    if ($isDefinition) {
-                        echo FilterGenerator::generateByDefinition($filterDefinition, $attributeName, $value);
-                    } else {
-                        if (!isset($filterDefinition['type'])) {
-                            $filterDefinition['type'] = 'text';
                         }
-                        echo FilterGenerator::generateByType($filterDefinition, $attributeName, $value);
                     }
+                    if (property_exists($filterModel, $attributeNameCamelCase)) {
+                        $value = $propertyAccessor->getValue($filterModel, $attributeNameCamelCase);
+                        $filterDefinition = $attributeEntity->getFilter();
+                        $isDefinition = is_array($filterDefinition) && isset($filterDefinition['class']) || is_string($filterDefinition);
+                        if ($isDefinition) {
+                            echo FilterGenerator::generateByDefinition($filterDefinition, $attributeName, $value);
+                        } else {
+                            if (!isset($filterDefinition['type'])) {
+                                $filterDefinition['type'] = 'text';
+                            }
+                            echo FilterGenerator::generateByType($filterDefinition, $attributeName, $value);
+                        }
+                    }
+                    ?>
+                </th>
+            <?php endforeach; ?>
+        </tr>
+    </form>
+
+    <script>
+
+        window.filterForm = {
+            submit: function (self) {
+                if (typeof self === 'undefined') {
+                    var form = $('#collection-filter-form');
+                } else {
+                    var form = self.form;
                 }
-                ?>
-            </th>
-        <?php endforeach; ?>
-    </tr>
-</form>
-
-<script>
-
-    window.filterForm = {
-        submit: function (self) {
-            if(typeof self === 'undefined') {
-                var form = $('#collection-filter-form');
-            } else {
-                var form = self.form;
+                $(form).submit();
+            },
+            submitOnKeyDown: function (self, event) {
+                if (event.keyCode === 13) {
+                    this.submit(self);
+                }
             }
-            $(form).submit();
-        },
-        submitOnKeyDown: function (self, event) {
-            if (event.keyCode === 13) {
-                this.submit(self);
-            }
-        }
-    };
+        };
 
-</script>
+    </script>
+
+<?php endif; ?>

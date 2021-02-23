57573379be3704ccb7800fe74d3c832950ac6bd2
Vitaliy <theyamshikov@yandex.ru>
2021-02-23 18:18:48 +0600

    upd

---
+++ src/Symfony4/MicroApp/layouts/blocks/head.php
@@ -0,0 +1,11 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<!-- Fontawesome CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<title><?= $title ?? '' ?></title>

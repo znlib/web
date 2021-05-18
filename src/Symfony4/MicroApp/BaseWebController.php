0ebd4a23246a7daf9d0c8de76a45aff573909be5
Vitaliy <theyamshikov@yandex.ru>
2021-05-18 20:34:37 +0600

    upd

--- src/Symfony4/MicroApp/BaseWebController.php
+++ src/Symfony4/MicroApp/BaseWebController.php
@@ -2,7 +2,10 @@
 
 namespace ZnLib\Web\Symfony4\MicroApp;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use ZnCore\Base\Helpers\LoadHelper;
 use ZnLib\Rest\Web\Controller\BaseCrudWebController;
 use ZnLib\Web\View\View;
@@ -49,5 +52,32 @@ abstract class BaseWebController
             ]);
         }
         return new Response($content);
+    }
+
+    protected function redirectToRoute(string $route, array $parameters = [], int $status = 302): RedirectResponse
+    {
+        return $this->redirect($this->generateUrl($route, $parameters), $status);
+    }
+
+    protected function redirectToHome(int $status = 302): RedirectResponse
+    {
+        return $this->redirect('/', $status);
+    }
+
+    protected function redirectToBack(Request $request): RedirectResponse
+    {
+        $referer = $request->headers->get('referer');
+        //$request->getSession()->setFlash('error', $exception->getMessage());
+        return new RedirectResponse($referer);
+    }
+
+    protected function redirect(string $url, int $status = 302): RedirectResponse
+    {
+        return new RedirectResponse($url, $status);
+    }
+
+    protected function generateUrl(string $route, array $parameters = [], int $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH): string
+    {
+        return $this->container->get('router')->generate($route, $parameters, $referenceType);
     }
 }

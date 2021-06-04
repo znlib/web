366ee228dd5228e9829e0d7a5fb7cb3ee9818c35
Vitaliy <theyamshikov@yandex.ru>
2021-06-04 21:27:09 +0600

    upd

---
+++ src/Widgets/Tab/views/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @var View $this
+ * @var array $items
+ * @var string $class
+ */
+
+use ZnLib\Web\View\View;
+
+?>
+
+<ul class="nav nav-tabs <?= $class ?>">
+    <?php foreach ($items as $item):
+        $title = $item['title'];
+        $isActive = $item['is_active'] ?? false;
+        $itemClass = $isActive ? 'active' : '';
+    ?>
+        <li class="nav-item">
+            <a class="nav-link <?= $itemClass ?>" href="<?= $item['url'] ?>" role="tab">
+                <?= $title ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>

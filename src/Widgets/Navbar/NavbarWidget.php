42ef19238d4bb44ac9c17d377256b48f5978e4d4
Vitaliy <theyamshikov@yandex.ru>
2021-08-07 17:29:28 +0600

    upd

---
+++ src/Widgets/Navbar/NavbarWidget.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ZnLib\Web\Widgets\Navbar;
+
+use ZnLib\Web\Widgets\Base\BaseWidget2;
+
+class NavbarWidget extends BaseWidget2
+{
+
+    public $leftMenu;
+    public $rightMenu;
+
+    public function run(): string
+    {
+        return $this->render('index', [
+            'leftMenu' => $this->leftMenu,
+            'rightMenu' => $this->rightMenu,
+        ]);
+    }
+
+}

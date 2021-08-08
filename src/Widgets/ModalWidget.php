2b6f8a6d8e8caf7a6568985273abc4ca10e1fbaf
Vitaliy <theyamshikov@yandex.ru>
2021-08-08 14:47:33 +0600

    upd

--- src/Widgets/ModalWidget.php
+++ src/Widgets/ModalWidget.php
@@ -3,39 +3,75 @@
 namespace ZnLib\Web\Widgets;
 
 use ZnLib\Web\Widgets\Base\BaseWidget;
+use ZnLib\Web\Widgets\Base\BaseWidget2;
 
-class ModalWidget extends BaseWidget
+class ModalWidget extends BaseWidget2
 {
 
     public $tagId;
     public $header;
     public $body;
     public $footer;
+    public $centered = false;
 
-    public function render(): string
+    public function run(): string
     {
         return '
-        <div class="modal fade" id="' . $this->tagId . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                        ' . $this->header . '
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ' . $this->body . '
-                </div>
-                <div class="modal-footer">
-                    ' . $this->footer . '
+            <div class="modal fade" id="' . $this->tagId . '" tabindex="-1" role="dialog" aria-labelledby="' . $this->tagId . 'Label" aria-hidden="true">
+                <div class="' . $this->generateDialogCssClass() . '" role="document">
+                    <div class="modal-content">
+                        ' . $this->generateHeaderHtml() . '
+                        ' . $this->generateBodyHtml() . '
+                        ' . $this->generateFooterHtml() . '
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
         ';
     }
 
+    protected function generateDialogCssClass(): string
+    {
+        $dialogCssClass = 'modal-dialog';
+        if ($this->centered) {
+            $dialogCssClass .= ' modal-dialog-centered';
+        }
+        return $dialogCssClass;
+    }
+
+    protected function generateCloseButtonHtml(): string
+    {
+        return '
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>';
+    }
+
+    protected function generateHeaderHtml(): string
+    {
+        return '
+            <div class="modal-header">
+                <h5 class="modal-title" id="' . $this->tagId . 'Label">
+                    ' . $this->header . '
+                </h5>
+                '. $this->generateCloseButtonHtml() .'
+            </div>';
+    }
+
+    protected function generateBodyHtml(): string
+    {
+        return '
+            <div class="modal-body">
+                ' . $this->body . '
+            </div>
+        ';
+    }
+
+    protected function generateFooterHtml(): string
+    {
+        return '
+            <div class="modal-footer">
+                ' . $this->footer . '
+            </div>
+        ';
+    }
 }

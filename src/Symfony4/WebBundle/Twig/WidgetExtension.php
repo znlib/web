9c4e1ca75d1f19ec34be61841d44cbe40f2320f4
Vitaliy <theyamshikov@yandex.ru>
2022-05-08 20:22:47 +0600

    upd

--- src/Symfony4/WebBundle/Twig/WidgetExtension.php
+++ src/Symfony4/WebBundle/Twig/WidgetExtension.php
@@ -2,12 +2,10 @@
 
 namespace ZnLib\Web\Symfony4\WebBundle\Twig;
 
-use ZnCore\Base\Helpers\DiHelper;
-use ZnLib\Web\Widgets\Interfaces\WidgetInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Psr\Container\ContainerInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
-use Psr\Container\ContainerInterface;
+use ZnLib\Web\Widgets\Interfaces\WidgetInterface;
 
 class WidgetExtension extends AbstractExtension
 {
@@ -30,7 +28,8 @@ class WidgetExtension extends AbstractExtension
     public function widget(string $widgetClass, array $params = [])
     {
         /** @var WidgetInterface $widget */
-        $widget = DiHelper::make($widgetClass, $this->container);
+//        $widget = DiHelper::make($widgetClass, $this->container);
+        $widget = $this->container->get($widgetClass);
         foreach ($params as $paramName => $paramValue) {
             $widget->{$paramName} = $paramValue;
         }

4210e2c4a84eb146a38b85aac2aebcd1b5546ca1
ZnDeveloper <zndev@ya.ru>
2022-06-15 23:40:33 +0600

    upd

--- src/Symfony4/HttpKernel/BaseHttpKernel.php
+++ src/Symfony4/HttpKernel/BaseHttpKernel.php
@@ -4,6 +4,8 @@ namespace ZnLib\Web\Symfony4\HttpKernel;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -14,6 +16,34 @@ abstract class BaseHttpKernel implements HttpKernelInterface, TerminableInterfac
 {
 
     use EventDispatcherTrait;
+
+    protected $requestStack;
+
+    /**
+     * Filters a response object.
+     *
+     * @throws \RuntimeException if the passed object is not a Response instance
+     */
+    protected function filterResponse(Response $response, Request $request, int $type): Response
+    {
+        $event = new ResponseEvent($this, $request, $type, $response);
+        $this->getEventDispatcher()->dispatch($event, KernelEvents::RESPONSE);
+        $this->finishRequest($request, $type);
+        return $event->getResponse();
+    }
+
+    /**
+     * Publishes the finish request event, then pop the request from the stack.
+     *
+     * Note that the order of the operations is important here, otherwise
+     * operations such as {@link RequestStack::getParentRequest()} can lead to
+     * weird results.
+     */
+    private function finishRequest(Request $request, int $type)
+    {
+        $this->getEventDispatcher()->dispatch(new FinishRequestEvent($this, $request, $type), KernelEvents::FINISH_REQUEST);
+        $this->requestStack->pop();
+    }
 
     /**
      * {@inheritdoc}

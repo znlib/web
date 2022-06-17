e2128b2924b3bccb5b1ea20cb3f58d97df83391b
ZnDeveloper <zndev@ya.ru>
2022-06-18 00:11:18 +0600

    upd

--- src/Symfony4/Libs/BundleLoaders/SymfonyRoutesLoader.php
+++ src/Symfony4/Libs/BundleLoaders/SymfonyRoutesLoader.php
@@ -6,40 +6,37 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use Symfony\Component\Routing\RouteCollection;
-use ZnCore\Base\Libs\App\Interfaces\ConfigManagerInterface;
 use ZnCore\Base\Libs\App\Loaders\BundleLoaders\BaseLoader;
 
 class SymfonyRoutesLoader extends BaseLoader
 {
 
-    /*public function __construct(ConfigManagerInterface $configManager)
+    private function createRoutingConfigurator($routes): RoutingConfigurator
     {
-        $this->setConfigManager($configManager);
-    }*/
 
-    public function loadAll(array $bundles): array
-    {
-        /** @todo костыль */
-        $this->container->singleton(RouteCollection::class, RouteCollection::class);
-        $routes = $this->container->get(RouteCollection::class); //new RouteCollection();
-//dd($routes);
         $fileLocator = new FileLocator();
         $fileLoader = new PhpFileLoader($fileLocator);
         $routingConfigurator = new RoutingConfigurator($routes, $fileLoader, __FILE__, __FILE__);
+        return $routingConfigurator;
+    }
 
+    public function loadAll(array $bundles): array
+    {
+        $routes = $this->container->get(RouteCollection::class); //new RouteCollection();
+        $routingConfigurator = $this->createRoutingConfigurator($routes);
         foreach ($bundles as $bundle) {
             $loadedConfig = $this->load($bundle);
             if ($loadedConfig) {
                 foreach ($loadedConfig as $configFile) {
                     $closure = include $configFile;
-                    $closure($routingConfigurator);
+                    call_user_func($closure, $routingConfigurator);
+//                    $closure($routingConfigurator);
                 }
             }
         }
-        $config['routeCollection'] = $routes;
-        //if($this->hasConfigManager()) {
-            $this->getConfigManager()->set('routeCollection', $routes);
-        //}
-        return $config;
+
+//        $config['routeCollection'] = $routes;
+//        $this->getConfigManager()->set('routeCollection', $routes);
+        return [];
     }
 }
